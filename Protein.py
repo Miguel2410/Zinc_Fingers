9b1794a692ffe93f3ba77e2a99228578c2cfcd12
RMolina93 <ruben.molina-fernandez@upf.edu>
2019-11-13 12:09:37 +0100

    Defined class protein

--- Protein.py
+++ Protein.py
@@ -2,6 +2,19 @@
 class Protein:
 
    def __init__(self):
-      self.algo = None
-      
+      self.C2H2 = None
+      self.C2WH2 = None
+      self.GATA3 = None
+      self.CCHC = None
+      self.ZN2C6 = None
+      self.zinc = None
+      self.prot_len = None
+      self.pos = None
+      self.num_chain = None
+      self.hys_cys = None
+
+   def distance(self, protein_2):
+      print ("Work in process.")
+
+   
    
