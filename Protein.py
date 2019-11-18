f3ea1cd389f612e79dc67e3f0a726ca4d336da06
jordibusoms <39164889+jordibusoms@users.noreply.github.com>
2019-11-18 22:21:00 +0100

    Added atribute *args to distance

--- Protein.py
+++ Protein.py
@@ -14,9 +14,16 @@ class Protein:
       self.num_chain = num_chain
       self.hys_cys = hys_cys
 
-   def distance(self, protein_2):
-      spatial_pos = [self.C2H2, self.C2WH2, self.GATA3, self.CCHC, self.ZN2C6, self.zinc, self.prot_len, self.pos, self.num_chain, self.hys_cys]
-      spatial_pos_2  = [protein_2.C2H2, protein_2.C2WH2, protein_2.GATA3, protein_2.CCHC, protein_2.ZN2C6, protein_2.zinc, protein_2.prot_len, protein_2.pos, protein_2.num_chain, protein_2.hys_cys]
+   def get_atribtes(self, *args):
+	   keys=[x for x in self.__dict__.keys() if x in args]
+	   values=[ self.__dict__[key] for key in keys]
+	   return values
+   
+   def distance(self, protein_2, *args):
+      if args==tuple():
+         args = [x for x in self.__dict__.keys() if x not in ("name","__init__","get_atributes", "distance", "toTupple")]
+      spatial_pos = self.get_atribtes(*args)
+      spatial_pos_2  = protein_2.get_atribtes(*args)
       subs = ([a - b for a, b in zip(spatial_pos, spatial_pos_2)])
       abs_list = [abs(ele) for ele in subs] 
       manhattan_distance = sum(abs_list)
